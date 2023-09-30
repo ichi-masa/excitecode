6a75ff333d62f7756e67337f17c88dc4ad83cdad
ichi-masa <smac.girl@gmail.com>
2023-10-01 06:39:20 +0900

    1001_共通パーツ

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,13 @@
 
 jQuery(function ($) { // この中であればWordpressでも「$」が使用可能になる
 
+        // ハンバーガーメニュー //
+        $(function () {
+            $('.hamburger').on('click', function () {
+                $(this).toggleClass('active');
+                $('.header').toggleClass('active');
+                return false;
+            });
+        });
 
 });
