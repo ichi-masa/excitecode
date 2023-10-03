1ffba0a6051b5b926850b2685087def6ee5faddd
ichi-masa <smac.girl@gmail.com>
2023-10-03 16:04:12 +0900

    1003_mv,header,footer

--- src/js/script.js
+++ src/js/script.js
@@ -1,13 +1,42 @@
 
 jQuery(function ($) { // この中であればWordpressでも「$」が使用可能になる
 
-        // ハンバーガーメニュー //
-        $(function () {
-            $('.hamburger').on('click', function () {
-                $(this).toggleClass('active');
-                $('.header').toggleClass('active');
-                return false;
-            });
+    // ハンバーガーメニュー //
+    $(function () {
+        $('.hamburger').on('click', function () {
+            $(this).toggleClass('active');
+            $('.header').toggleClass('active'); // header //
+            $('.header__circle-bg').toggleClass('active');
+            $('.sp-nav').toggleClass('active'); // ドロワーメニュー //
+            $('body').toggleClass('no-scroll'); // ドロワーメニュー下の背景固定 //
+
+
+            let img = $('.header__logo-color').children('img');
+            let currentSrc = img.attr('src');
+            let newSrc = '';
+
+            if (currentSrc.includes('white')) {
+                newSrc = currentSrc.replace('white', 'navy');
+            } else {
+                newSrc = currentSrc.replace('navy' , 'white');
+            }
+
+            img.attr('src', newSrc);
         });
+    });
+
+
+
+    // mainview スライダー（Swiper）//
+    const mvSwiper = new Swiper(".mainview__Swiper", {
+        loop: true,
+        effect: 'slide',
+        direction: 'vertical',
+        autoplay: {
+            delay: 2000,
+            disableOnInteraction: false
+        },
+        speed: 2000,
+    });
 
 });
